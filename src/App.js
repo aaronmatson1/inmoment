5d466e1075ad145a79f8e3cddea0c72111df26a2
matsonaaron1 <matsonaaron1@gmail.com>
2021-03-02 23:01:39 -0700

    Updated Arrows and Icons/ Fixing More CSS

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 import tablet from './assets/tablet.png'
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import Button from 'react-bootstrap/Button'
 
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './App.css';
+
 
 
 function App() {
@@ -21,52 +23,45 @@ function App() {
 
       <p>Customer data doesn't only come in the form of surveys; it's also in chats, call transcripts, social media, online reviews, and other sources. No matter what form your CX data takes, we can extract meaning and help you drive action.</p>
 
+
         <Accordion>
           <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Feedback Lifecycle Management
-              </Accordion.Toggle>
-            </Card.Header>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+            Feedback Lifecycle Management <i class="fa fa-arrow-down"></i>
+            </Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
-              <Card.Body>
-              <h4> Lorem Ipsum</h4>
+              <Card.Body><h4> Lorem Ipsum</h4>
               <p>Exercitation in fugiat est ut ad ea cupidatat ut in
                         cupidatat occaecat ut occaecat consequat est minim minim
                         esse tempor laborum consequat esse adipisicing eu
                         reprehenderit enim.</p>
-                        <a class="btn" href="#">See more work</a>
-                        </Card.Body>
+                        <Button variant='primary' size="sm">See More</Button></Card.Body>
             </Accordion.Collapse>
           </Card>
           <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                Integrations
-              </Accordion.Toggle>
-            </Card.Header>
+            <Accordion.Toggle as={Card.Header} eventKey="1">
+            Integrations <i class="fa fa-arrow-down"></i>
+            </Accordion.Toggle>
             <Accordion.Collapse eventKey="1">
               <Card.Body><h4> Lorem Ipsum</h4>
               <p>Exercitation in fugiat est ut ad ea cupidatat ut in
                         cupidatat occaecat ut occaecat consequat est minim minim
                         esse tempor laborum consequat esse adipisicing eu
                         reprehenderit enim.</p>
-                        <a class="btn" href="#">See more work</a></Card.Body>
+                        <Button variant='primary' size="sm">See More</Button></Card.Body>
             </Accordion.Collapse>
           </Card>
           <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                Multimedia Feedback
-              </Accordion.Toggle>
-            </Card.Header>
+            <Accordion.Toggle as={Card.Header} eventKey="2">
+            Multimedia Feedback <i class="fa fa-arrow-down"></i>
+            </Accordion.Toggle>
             <Accordion.Collapse eventKey="2">
               <Card.Body><h4> Lorem Ipsum</h4>
               <p>Exercitation in fugiat est ut ad ea cupidatat ut in
                         cupidatat occaecat ut occaecat consequat est minim minim
                         esse tempor laborum consequat esse adipisicing eu
                         reprehenderit enim.</p>
-                        <a class="btn" href="#">See more work</a></Card.Body>
+                        <Button variant='primary' size="sm">See More</Button></Card.Body>
             </Accordion.Collapse>
           </Card>
         </Accordion>
